92932a287de7bcf058940b2097a3b01b67ffb532
mustang-ar <hernan.yamakawa@gmail.com>
2021-03-07 22:41:57 -0300

    Remove helm upper version constraint

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   required_providers {
     aws        = ">= 2.0, < 4.0"
-    helm       = ">= 1.0, < 1.4.0"
+    helm       = ">= 1.0"
     kubernetes = ">=1.10.0"
     local      = "~> 1.2"
     null       = "~> 2.0"
